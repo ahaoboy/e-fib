f5332c6f11aa5f47a0556f2ed640df711dd80d64
阿豪boy <504595380@qq.com>
2021-11-27 13:45:34 +0800

    fix: typo

--- apps/core/src/index.ts
+++ apps/core/src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   return a + b;
 };
-export const fib = (n: number) => {
+export const fib = (n: number): number => {
   return n < 2 ? n : fib(n - 1) + fib(n - 2);
 };
